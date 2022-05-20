a0e4e3f9bcc68b6946ddd5acaeb14b3c3e1e9678
GuillaumeMartinez93 <104209071+GuillaumeMartinez93@users.noreply.github.com>
2022-05-20 16:43:06 +0200

    Update Dashboard_p7.py

--- Dashboard_p7.py
+++ Dashboard_p7.py
@@ -16,71 +16,11 @@ st.set_page_config(layout="wide")
 
 def load_data():
 
-	file_name='Model'
-    open_file = open(file_name, "rb")
-    model_loaded = pickle.load(open_file)
-    open_file.close()
-
 	file_name='DataSet'
-    open_file = open(file_name, "rb")
-    df_test = pickle.load(open_file)
-    open_file.close()
-
-	explainer = shap.TreeExplainer(model_loaded)
-	shap_values = explainer.shap_values(df_test)[1]
-	exp_value=explainer.expected_value[1]
-
-return df_test,shap_values,exp_value
-
-def score_viz(df_test,client,exp_value,shap_values):
-
-	st.title('Dashboard Pret à dépenser')
-	st.subheader('Visualisation score')
-
-    json_object = json.dumps(client,indent=4) 
-    url = "https://apphomecredit.herokuapp.com/predict"
-    headers = {"Content-Type":"application/json"}
-    x = requests.post(url, headers=headers ,data = json_object )
-	score=float(x.text.split("[")[1].split("]")[0])
-    if score> :
-        result='Approved'
-    else :
-        result='Rejected'
-
-	fig = go.Figure(go.Indicator(
-	mode = "gauge+number+delta",
-    value = score,
-    number = {'font':{'size':48}},
-    domain = {'x': [0, 1], 'y': [0, 1]},
-    title = {'text': result.tolist(), 'font': {'size': 28, 'color':color(result)}},
-    delta = {'reference': 0.48, 'increasing': {'color': "red"},'decreasing':{'color':'green'}},
-    gauge = {
-        'axis': {'range': [0,1], 'tickcolor': color(result)},
-        'bar': {'color': color(result)},
-        'steps': [
-            {'range': [0,0.48], 'color': 'lightgreen'},
-            {'range': [0.48,1], 'color': 'lightcoral'}],
-        'threshold': {
-            'line': {'color': "black", 'width': 5},
-            'thickness': 1,
-            'value': 0.48}}))
-
-
-	st.plotly_chart(fig)
-
-	st_shap(shap.force_plot(exp_value, shap_values[client], features = df_test.iloc[client], feature_names=df_test.columns, figsize=(12,5)))
-
-def color(pred):
-	if pred=='Approved':
-		col='Green'
-	else :
-		col='Red'
-	return col
-
-def st_shap(plot, height=None):
-	shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-	components.html(shap_html, height=height)
-
+	open_file = open(file_name, "rb")
+	db_test = pickle.load(open_file)
+	open_file.close()
+	return db_test
 
 def tab_client(db_test):
 
@@ -114,23 +54,8 @@ def tab_client(db_test):
 	st.dataframe(db_display)
 	st.markdown("**Total clients correspondants: **"+str(len(db_display)))
 
-def get_client(db_test):
-	client=st.sidebar.selectbox('Client',db_test['SK_ID_CURR'])
-	idx_client=db_test.index[db_test['SK_ID_CURR']==client][0]
-	return client,idx_client
-
-def infos_client(db_test,client,idx_client):
-	"""Affichage des infos du client sélectionné dans la barre latérale"""
-	st.sidebar.markdown("**ID client: **"+str(client))
-	st.sidebar.markdown("**Sexe: **"+db_test.loc[idx_client,'CODE_GENDER'])
-	st.sidebar.markdown("**Statut familial: **"+db_test.loc[idx_client,'NAME_FAMILY_STATUS'])
-	st.sidebar.markdown("**Enfants: **"+str(db_test.loc[idx_client,'CNT_CHILDREN']))
-	st.sidebar.markdown("**Age: **"+str(db_test.loc[idx_client,'YEARS_BIRTH']))	
-	st.sidebar.markdown("**Statut pro.: **"+db_test.loc[idx_client,'NAME_INCOME_TYPE'])
-	st.sidebar.markdown("**Niveau d'études: **"+db_test.loc[idx_client,'NAME_EDUCATION_TYPE'])
-
 def main():
-    df_test,shap_values,exp_value=load_data()
+    db_test=load_data()
     PAGES = [
 	    "Tableau clientèle",
 	    "Visualisation score",
@@ -145,9 +70,7 @@ def main():
 	if selection=="Tableau clientèle":
 		tab_client()
 	if selection=="Visualisation score":
-		client,idx_client=get_client(db_test)
-		infos_client(db_test,client,idx_client)
-		score_viz(lgbm,df_test,idx_client,exp_value,shap_values)
+		pass
 	if selection=="Comparaison clientèle":
         pass
 
